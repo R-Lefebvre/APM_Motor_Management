80c120cdc82092fa82f87ecd5143a2cd4c784361
Robert Lefebvre <robert.lefebvre@gmail.com>
2015-02-25 17:14:31 -0500

    Temperature: Cleanup, commenting.

--- libraries/Temperature/Temperature.cpp
+++ libraries/Temperature/Temperature.cpp
@@ -1,10 +1,12 @@
 #include <Temperature.h>
 #include <Arduino.h>
 
+// Constructor
 Temperature::Temperature(uint8_t pin){
     pin_assignment = pin;
 }
 
+// To be called at 100Hz, takes measurement and applies simple LPF
 void Temperature::take_reading(){
     // This does a simple low-pass filter on the temperature reading which can be quite noisy
     filtered_V = 0.99*filtered_V + 0.01*(3.3 * analogRead(pin_assignment)/1023);
--- libraries/Temperature/Temperature.h
+++ libraries/Temperature/Temperature.h
@@ -5,13 +5,15 @@
 
 class Temperature{
 
-    uint8_t pin_assignment;
-    float filtered_V;
-
     public:
-        Temperature(uint8_t);
-        float get_temp_V(){return filtered_V;};
-        void take_reading();
+        // Methods
+        Temperature(uint8_t);                       // Constructor
+        float get_temp_V(){return filtered_V;};     // Return filtered voltage reading
+        void take_reading();                        // To be called at 100Hz, takes measurement and applies simple LPF
+
+    private:
+        uint8_t pin_assignment;                     // Analog input pin used on hardware
+        float filtered_V;                           // filtered voltage
 
 };
 
